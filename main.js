360ea0050cc2f7c5bc8c29869de4fbb1875d0274
Elbastito <naitsabes_ochoa@hotmail.com>
2021-09-01 12:28:35 -0500

    Creacion de la primer clase

---
+++ main.js
@@ -0,0 +1,10 @@
+class Contacto{
+    constructor(n,app,tel,ca,ext,col){
+        this.Nombre=n;
+        this.Apellidos=app;
+        this.Telefono=tel;
+        this.Calle=ca;
+        this.Exterior=ext;
+        this.Colonia=col;
+    }
+}
