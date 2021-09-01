304de049ec670579463e71e9fd7adc8ee161d693
Elbastito <naitsabes_ochoa@hotmail.com>
2021-09-01 18:30:58 -0500

    ya no se que hacer

--- main.js
+++ main.js
@@ -9,21 +9,24 @@ class EstrCon{
     }
 }
 
-let contacto= function (){
+let contacto = function (){
     let cbar=new EstrCon(a=prompt("Nombre"),b=prompt("Apellido paterno"), c=prompt("Telefono"),d=prompt("Calle"),e=prompt("No. exterior"),f=prompt("Colonia"));
     return cbar;
 }
 
+
 let Agenda=[];
+let med=new EstrCon();
 let o=1;
+
 do {
-    o=Number(prompt("***BENVENDIO A TU AGENDA***\n1.Agregar contanto\n2.Buscar contacto\n0.Salir"));
+    o=Number(prompt("***BENVENDIO A TU AGENDA***\n1-->Agregar contanto\n2-->Buscar contacto\n0-->Salir"));
     switch (o) {
         case 1:
             Agenda.push(contacto());
             break;
         case 2:
-            Agenda.filter();
+
             break;
         case 3:
             break;
